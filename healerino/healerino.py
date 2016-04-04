39e51cbc20bbd310a212b93fc7c30f4de78e9b7b
Hugo Cachitas <hcachitas@gmail.com>
2016-04-04 17:15:00 +0100

    UI prototype

--- healerino/healerino.py
+++ healerino/healerino.py
@@ -3,3 +3,22 @@ Healerino
 =========
 
 """
+
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.progressbar import ProgressBar
+from kivy.uix.boxlayout import BoxLayout
+
+
+class Game(BoxLayout):
+    pass
+
+
+class Healerino(App):
+
+    def build(self):
+        return Game()
+
+
+if __name__ == '__main__':
+    Healerino().run()
